8688b0d28ad32afdf68fd7a68ef548e2cfafb3c0
ninaagosthazi <nagostha@calpoly.edu>
2026-05-04 00:20:38 -0700

    commit 1

--- proj2.py
+++ proj2.py
@@ -1,12 +1,34 @@
+import sys
 import csv
 import math
 from dataclasses import dataclass
 from typing import *
 
-#testing 
+sys.setrecursionlimit(10000)
+
 # Put your data definitions first!
 
-# ...
+# Row dataclass
+@dataclass(frozen=True)
+class Row:
+    country: str
+    year: int
+    electricity_and_heat_co2_emissions: float | None
+    electricity_and_heat_co2_emissions_per_capita: float | None
+    energy_co2_emissions: float | None
+    energy_co2_emissions_per_capita: float | None
+    total_co2_emissions_excluding_lucf: float | None
+    total_co2_emissions_excluding_lucf_per_capita: float | None
+
+# Node dataclass
+@dataclass(frozen=True)
+class Node:
+    value: Row
+    next: Node | None
+
+
+
+
 
 # Then your functions.
 
